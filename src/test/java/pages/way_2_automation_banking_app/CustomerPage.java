c5ef4e377a9879aeec6e85279e33f718cfdd7770
teya-teya <teya.teya89@yandex.ru>
2026-02-27 00:41:18 +0300

    Задача U1. fix

--- src/test/java/pages/way_2_automation_banking_app/CustomerPage.java
+++ src/test/java/pages/way_2_automation_banking_app/CustomerPage.java
@@ -13,14 +13,12 @@ public class CustomerPage {
     public WebDriver driver;
     public WebDriverWait wait;
     public WebSteps webSteps;
-    public AccountPage accountPage;
     public WebChecks webChecks;
 
     public CustomerPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
         this.webSteps = new WebSteps(driver, wait);
-        this.accountPage = new AccountPage(driver, wait);
         this.webChecks = new WebChecks(driver, wait);
         PageFactory.initElements(driver, this);
     }
