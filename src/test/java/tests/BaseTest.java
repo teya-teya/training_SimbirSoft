b6062829ea81baaa6bc3c2b52cde77d780836e9d
teya-teya <teya.teya89@yandex.ru>
2026-03-01 02:06:41 +0300

    Задача U3. DataProvider

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,16 +1,18 @@
 package tests;
 
 import io.qameta.allure.Step;
+import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import utils.ConfigReader;
 
 import java.time.Duration;
 
-//@Listeners({AllureTestNg.class})
+@Listeners({AllureTestNg.class})
 public class BaseTest {
     protected WebDriver driver;
     protected WebDriverWait wait;
