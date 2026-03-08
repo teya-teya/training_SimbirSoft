8eb6258c408e84a5b515b9943c4706ba8f27adb2
Anastasia <142177100+teya-teya@users.noreply.github.com>
2026-03-08 21:37:57 +0300

    Задача U13. Basic auth (#14)
    * Задача U0. Настройка окружения
    
    * Задача U1. 1-3
    
    * Задача U1. 5
    
    * Задача U1. 4
    
    * Задача U1. 4
    
    * Задача U1. fix
    
    * Задача U1. fix
    
    * Задача U1. fix
    
    * Задача U2. Allure
    
    * Задача U2. fix
    
    * Задача U2. fix
    
    * Задача U2. fix
    
    * Задача U3. DataProvider
    
    * Задача U3. DataProvider
    
    * Задача U4. Screenshots
    
    * Задача U4. Screenshots
    
    * Задача U4. Screenshots
    
    * Задача U5. Cookies
    
    * Задача U6. JavaScriptExecutor
    
    * Задача U7. Параллельный запуск тестов
    
    * Задача U7. Параллельный запуск тестов
    
    * Задача U7. Параллельный запуск тестов. Исправление
    
    * Задача U7. Параллельный запуск тестов. Исправление
    
    * Задача U8. Перезапуск упавших тестов
    
    * Задача U9. Браузеры
    
    * Задача U9. Браузеры
    
    * Задача U10. Drag n Drop (IFrame)
    
    * Задача U11. Tabs
    
    * Задача U11. Tabs
    
    * Задача U12. Alerts
    
    * Задача U13. Basic auth
    
    * Задача U13. Basic auth
    
    * Задача U13. Basic auth
    
    * Задача U13. Basic auth

---
+++ src/test/java/tests/HttpAuthenticationTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import base.WebSteps;
+import enums.URL;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HttpAuthenticationPage;
+
+@Epic("UI тесты")
+@Feature("Базовая аутентификация")
+public class HttpAuthenticationTest extends BaseTest{
+    WebSteps webSteps;
+    HttpAuthenticationPage httpAuthenticationPage;
+
+    @BeforeMethod
+    void setUp() {
+        webSteps = new WebSteps(driver, wait);
+        httpAuthenticationPage = new HttpAuthenticationPage(driver, wait);
+
+        webSteps.goToPageFullLink(URL.HTTP_AUTHENTICATION.getUrl());
+    }
+
+    @Story("Проверка базовой аутентификации")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(groups = "auth", description = "Проверка успешной авторизации через Basic Auth")
+    void checkSuccessfulBasicAuth() {
+        httpAuthenticationPage.scrollAndClickBtnDisplayImage()
+                .enableBasicAuth(System.getenv("LOGIN_HTTP"), System.getenv("PASSWORD_HTTP"))
+                .checkSuccessAuthentication();
+    }
+}
